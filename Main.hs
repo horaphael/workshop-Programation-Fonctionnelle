ed72f3bef88841dd0f48463e0dd474265ea751bf
rapael hoarau <raphael.hoarau@epitech.eu>
2024-01-29 11:39:13 +0400

    step 1 2 3 4

--- Main.hs
+++ Main.hs
@@ -3,5 +3,11 @@ doOp a b = a + b
 
 main :: IO ()
 main = do
-    x <- getLine
-    print (x)
+    input1 <- getLine
+    let num1 = read input1
+
+    input2 <- getLine
+    let num2 = read input2
+
+    let result = doOp num1 num2
+    print(result)
